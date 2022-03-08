e3bc462367f2b50a91bb8b67ab272a042cd9c9cb
jlozanop42 <jlozanop42@gmail.com>
2022-03-08 13:05:07 -0500

    Adding service interfaces and base Entity model

--- pet-clinic-data/src/main/java/jslozano/petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/jslozano/petclinic/model/Person.java
@@ -1,6 +1,6 @@
 package jslozano.petclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
     private String firstName;
     private String lastName;
 
---
+++ pet-clinic-data/src/main/java/jslozano/petclinic/services/VetService.java
@@ -0,0 +1,13 @@
+package jslozano.petclinic.services;
+
+import jslozano.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+
+
+}
