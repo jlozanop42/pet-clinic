b45c1081bb6e093abbe546418916cfadfdfc33ac
jlozanop42 <jlozanop42@gmail.com>
2022-03-16 22:05:19 -0500

    Added specialities, added to vet. Closes #22

---
+++ pet-clinic-data/src/main/java/jslozano/petclinic/model/Speciality.java
@@ -0,0 +1,9 @@
+package jslozano.petclinic.model;
+
+public class Speciality extends BaseEntity{
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+}
